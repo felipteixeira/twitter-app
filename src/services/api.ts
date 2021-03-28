2809c1fee44fc948bdc7a39a55da5c09fcd6424e
Felipe Teixeira <felipe.teixeira@willbank.com.br>
2021-03-28 11:46:54 -0300

    nlb edit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 
+const nlb = 'nlb-prod-f3d2c25d74505530.elb.us-east-1.amazonaws.com'
+
 export const api = {
     get(hashtag: string) {
         return axios.get(
-            `http://nlb-prod-41c99481a2337048.elb.us-east-1.amazonaws.com:1001/tweet?hashtag=${hashtag}`,
+            `${nlb}:1001/tweet?hashtag=${hashtag}`,
         ).then((result: AxiosResponse) => {
             return result.data
         }).catch((err) => {
